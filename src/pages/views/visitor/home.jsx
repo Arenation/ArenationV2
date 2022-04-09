75034b47815434263f75e3e0ab584d35491c87d8
Jose Ayazo <josemanuelayazo@gmail.com>
2022-04-08 22:23:01 -0500

    SignIn Modal creado (solo inputs de email y password)

--- src/pages/views/visitor/home.jsx
+++ src/pages/views/visitor/home.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import "../../../styles/components/home/body.css";
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import "../../../styles/fontstyles/textfonts.css";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 const Home = () => {
-
     const [locationSelected, setLocationSelected] = useState("");
     const [sportSelected, setSportSelected] = useState("");
 
@@ -14,16 +14,10 @@ const Home = () => {
         "Sahagún",
         "San Pelayo",
         "Cotorra",
-        "Planeta Rica"
+        "Planeta Rica",
     ];
 
-    const sport = [
-        "Fútbol",
-        "Fútbol Sala",
-        "Baloncesto",
-        "Voleibol",
-        "Tenis",
-    ];
+    const sport = ["Fútbol", "Fútbol Sala", "Baloncesto", "Voleibol", "Tenis"];
 
     const handleChangeLocation = (event) => {
         setLocationSelected(event.target.value);
@@ -32,70 +26,74 @@ const Home = () => {
         setSportSelected(event.target.value);
     };
 
-    return (<>
-        <div className="body_header">
-            <h1 style={{ 
-                    width: "40%", 
-                    textAlign: "center",
-                    color: "#070928",
-                    marginBottom: "2rem"
-                }}
-            >
-                Reserva escenarios deportivos cuando quieras y dónde quieras
-            </h1>
-            <div className="input-body">
-                <div style={{width: "100%"}}>
-                    <h5 className="input-body-h5">Ubicación</h5>
-                    <Select
-                        className="input-body-text"
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={locationSelected}
-                        onChange={handleChangeLocation}
-                    >
-                        {location.map((item) => {
-                            return <MenuItem value={item}>{item}</MenuItem>
-                        })}
-                    </Select>
+    return (
+        <>
+            <div className="body_header">
+                <h1
+                    style={{
+                        width: "40%",
+                        textAlign: "center",
+                        color: "#070928",
+                        marginBottom: "2rem",
+                    }}
+                >
+                    Reserva escenarios deportivos cuando quieras y dónde quieras
+                </h1>
+                <div className="input-body">
+                    <div style={{ width: "100%" }}>
+                        <h5 className="input-body-h5">Ubicación</h5>
+                        <Select
+                            className="input-body-text"
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={locationSelected}
+                            onChange={handleChangeLocation}
+                        >
+                            {location.map((item) => {
+                                return <MenuItem value={item}>{item}</MenuItem>;
+                            })}
+                        </Select>
+                    </div>
+                    <div style={{ width: "100%" }}>
+                        <h5 className="input-body-h5">Deporte</h5>
+                        <Select
+                            className="input-body-text"
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={sportSelected}
+                            onChange={handleChangeSport}
+                        >
+                            {sport.map((item) => {
+                                return <MenuItem value={item}>{item}</MenuItem>;
+                            })}
+                        </Select>
+                    </div>
+                    <div className="input-body-button">
+                        <button>
+                            <SearchOutlinedIcon />
+                        </button>
+                    </div>
                 </div>
-                <div style={{width: "100%"}}>
-                    <h5 className="input-body-h5">Deporte</h5>
-                    <Select
-                        className="input-body-text"
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={sportSelected}
-                        onChange={handleChangeSport}
-                    >
-                        {sport.map((item) => {
-                            return <MenuItem value={item}>{item}</MenuItem>
-                        })}
-                    </Select>
-                </div >
-                <div className="input-body-button">
-                    <button>
-                        <SearchOutlinedIcon/>
-                    </button>
-                </div>
+                <h1
+                    style={{
+                        marginTop: "3rem",
+                        width: "30%",
+                        textAlign: "center",
+                        color: "#070928",
+                    }}
+                >
+                    ¿Aún no sabes lo que buscas? ¡No hay problema!
+                </h1>
+                <button className="btn-primary">
+                    Ver todos los escenarios
+                </button>
             </div>
-            <h1 style={{
-                    marginTop: "3rem",
-                    width: "30%", 
-                    textAlign: "center",
-                    color: "#070928"
-                }}
-            >
-                ¿Aún no sabes lo que buscas? ¡No hay problema!
-            </h1>
-            <button className="btn-primary">
-                Ver todos los escenarios
-            </button>
-        </div>
-        <div className="body_header">
-            <h1>body</h1>
-        </div>
-        <div className="body_header">Footer</div>
-    </>);
+            <div className="body_header">
+                <h1>body</h1>
+            </div>
+            <div className="body_header">Footer</div>
+        </>
+    );
 };
 
 export default Home;
--- src/parts/NavBar.jsx
+++ src/parts/NavBar.jsx
@@ -1,28 +1,93 @@
 import React from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./style/NavBar.css";
+import "./style/SignModal.css";
 import logo from "../assets/images/navbar/logo.svg";
 import userIcon from "../assets/images/navbar/user_icon.svg";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import { InputMail, InputPassword, InputText } from "./components/inputs/Input";
+import { SolidButton } from "./components/buttons/Buttons";
 const NavBar = () => {
+    const [openModal, setOpenModal] = useState(false);
+    const handleOpenSignModal = () => setOpenModal(true);
+    const handleCloseSignModal = () => setOpenModal(false);
+
+    const boxStyle = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "90%",
+        maxWidth: 600,
+        bgcolor: "background.paper",
+        borderRadius: 4,
+        boxShadow: 24,
+        p: 4,
+    };
+
     return (
-        <div style={{ width: "100vw", height:"100vh", overflow:"hidden" }}>
+        <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
             <div className="nav-wrapper">
                 <div className="nav-flex-container">
                     <div className="nav-logo-wrapper">
                         <img src={logo} alt="arenation logo" id="nav-logo" />
                     </div>
                     <div className="nav-options-wrapper">
-                        <button className="outline-button">Ingresar</button>
+                        <button
+                            className="outline-button"
+                            onClick={handleOpenSignModal}
+                        >
+                            Ingresar
+                        </button>
+                        <Modal open={openModal} onClose={handleCloseSignModal}>
+                            <Box sx={boxStyle}>
+                                <SignForm />
+                            </Box>
+                        </Modal>
                         <div className="user-badge">
                             <img src={userIcon} alt="user icon" />
                         </div>
                     </div>
                 </div>
             </div>
-            <div style={{ marginTop: "3.4rem"}}>
+            <div style={{ marginTop: "3.4rem" }}>
                 {/* Aqui se va a renderizar el children */}
                 <Outlet />
             </div>
+        </div>
+    );
+};
+
+const SignForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleEmailChange = (event) => setEmail(event.target.value);
+    const handlePasswordChange = (event) => setPassword(event.target.value);
+
+    return (
+        <div className="sign-modal">
+            <h2>Ingresar</h2>
+            <hr />
+            <h3>Bienvenido a Arenation</h3>
+            <InputMail
+                label={"Correo electrónico"}
+                isRequired={true}
+                placeholder={"email@email.com"}
+                name={"email"}
+                value={email}
+                onChange={handleEmailChange}
+            />
+            <InputPassword
+                label={"Contraseña"}
+                isRequired={true}
+                placeholder={"Contraseña"}
+                name={"password"}
+                value={password}
+                onChange={handlePasswordChange}
+            />
+            <SolidButton text="Continuar" />
         </div>
     );
 };
